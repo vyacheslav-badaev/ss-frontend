728d477d55da03e15d3d6cbca5407e9f9baa1b83
Vyacheslav <vyacheslav.badaev@devit.group>
2022-01-13 19:28:31 +0000

    Убрал картинку из глобал.цсс

--- src/components/center-wrapper.js
+++ src/components/center-wrapper.js
@@ -10,6 +10,20 @@ function CenterWrapper({ children, className }) {
   )
 }
 const StyledCenterWrapper = styled(CenterWrapper)`
+  &::before {
+    content: '';
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #140028;
+    background-image: url('/static/images/topography.svg'),
+      linear-gradient(20deg, #140028, #141414);
+    background-size: 170px, auto;
+    background-repeat: repeat;
+    z-index: -1;
+  }
   .inner {
     height: 100vh;
     width: 100%;
--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -12,6 +12,20 @@ function Wrapper({ children, className }) {
   )
 }
 const StyledWrapper = styled(Wrapper)`
+  &::before {
+    content: '';
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #140028;
+    background-image: url('/static/images/topography.svg'),
+      linear-gradient(20deg, #140028, #141414);
+    background-size: 170px, auto;
+    background-repeat: repeat;
+    z-index: -1;
+  }
   .inner {
     max-width: 1024px;
     min-height: calc(100vh - 124px);
