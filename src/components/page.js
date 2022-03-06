f93c9401bddfad37cbc4a906302a62913c19bb6c
Vyacheslav <vyacheslav.badaev@devit.group>
2022-03-06 15:04:32 +0000

    Удалил emotion из проекта

--- src/components/page.js
+++ src/components/page.js
@@ -1,21 +1,5 @@
 import React, { useEffect } from 'react'
-import { ThemeProvider } from 'emotion-theming'
 import Meta from './meta'
-const theme = {
-  black: '#272727',
-  white: '#fcfcfc',
-  yellow: '#ffc600',
-  softViolet: '#6d47d9',
-  pink: '#f4c4f3',
-  red: '#f01',
-  lightGrey: '#eee',
-  darkGrey: '#aaa',
-  nightGrey: '#b8b8b8',
-  uiFont: 'PT Sans, sans-serif',
-  textFont: 'PT Serif, serif',
-  logoFont: 'Kaushan Script, cursive',
-  boxShadow: '0 1px 16px rgba(0, 0, 0, 0.25)',
-}
 function Page({ children }) {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
@@ -30,10 +14,10 @@ function Page({ children }) {
     }
   })
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Meta />
       {children}
-    </ThemeProvider>
+    </>
   )
 }
 export default Page
